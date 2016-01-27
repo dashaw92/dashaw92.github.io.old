638b2055e4707413bf439158b75650ada4ec2897
comp92 <dashaw92@gmail.com>
2016-01-27 14:50:27 -0500

    Started site script

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function onLink() {
-    
+function onLink(name) {
+    //TODO: Load file from disk? Or use html files?
+    //TODO: Figure this out
+    //TODO: Possible pushHistory here so backspace action
+    //returns to last viewed repo page instead of last
+    //website
+    document.getElementById("view").innerHTML = "TODO: " + name;
 }
